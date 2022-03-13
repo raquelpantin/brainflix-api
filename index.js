d027371adba664c31e7cd304ba0008fe54d6f77a
Raquel Pantin <raquel.pantin@gmail.com>
2022-03-12 19:05:52 -0500

    removed unused files and erased commented out code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,13 +7,6 @@ require("dotenv").config();
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(cors());
-
-// get data from heroku and convert to JSON
-// axios.get(`${API_URL}/videos${API_KEY}`).then((response) => {
-//   fs.writeFile("data/videos.json", JSON.stringify(response.data), () => {
-//     console.log("file succesfully written");
-//   });
-// });
 
 app.use("/videos", videosRoutes);
 
